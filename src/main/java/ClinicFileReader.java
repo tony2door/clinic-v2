082689d482de712446da57167d39e41af449d110
Tony <antoniu.tudor@trip-express.com>
2019-12-08 15:36:48 +0200

    implemented methods for human patients/problems. adding methods specific of the read type

--- src/main/java/ClinicFileReader.java
+++ src/main/java/ClinicFileReader.java
@@ -1,14 +1,27 @@
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
 public class ClinicFileReader implements ClinicReader{
     public List<AbstractPatient> readPatients() throws IOException, URISyntaxException {
+        List<String> strings = readFileByName("human_patients.txt");
+
+        return null;
+    }
+    public Map<Integer, String> readProblems() throws IOException, URISyntaxException {
+        List<String> strings = readFileByName("human_problems.txt");
+
         return null;
     }
 
-    public Map<Integer, String> readProblems() throws IOException, URISyntaxException {
-        return null;
+    private List<String>readFileByName(String fileName) throws URISyntaxException, IOException {
+        URI patients = ClassLoader.getSystemResource(fileName).toURI();
+        Path pathOfPatients = Paths.get(patients);
+        return Files.readAllLines(pathOfPatients);
     }
 }
