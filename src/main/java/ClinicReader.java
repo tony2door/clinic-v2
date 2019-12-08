fa2e9884dafa9f01109f6515a401f39eba1b123c
Tony <antoniu.tudor@trip-express.com>
2019-12-08 15:03:57 +0200

    added interface features: (list) read patients and (map) read problems

--- src/main/java/ClinicReader.java
+++ src/main/java/ClinicReader.java
@@ -1,2 +1,9 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+
 public interface ClinicReader {
+    List<AbstractPatient> readPatients() throws IOException, URISyntaxException;
+    Map<Integer, String> readProblems() throws IOException, URISyntaxException;
 }
