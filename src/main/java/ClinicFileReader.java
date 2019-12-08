ccb9c78605bc4710ebacd328ca043a0993fb6c28
AdrianIonutH <adriano_aih@yahoo.com>
2019-12-08 15:09:20 +0200

    ClinicFileReader implements ClinicReader

--- src/main/java/ClinicFileReader.java
+++ src/main/java/ClinicFileReader.java
@@ -1,2 +1,14 @@
-public class ClinicFileReader {
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+
+public class ClinicFileReader implements ClinicReader{
+    public List<AbstractPatient> readPatients() throws IOException, URISyntaxException {
+        return null;
+    }
+
+    public Map<Integer, String> readProblems() throws IOException, URISyntaxException {
+        return null;
+    }
 }
