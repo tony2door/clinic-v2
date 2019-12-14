eba58a03a794177f8ef94e5bbca5a07e68d0dd48
Oana Radu <raduoana124@yahoo.com>
2019-12-14 15:04:57 +0200

    added aditional constructor problemName+ getter

--- src/main/java/AbstractPatient.java
+++ src/main/java/AbstractPatient.java
@@ -2,6 +2,15 @@ public abstract class AbstractPatient {
     protected int patientId;
     protected String patientName;
     protected int problemID;
+    protected String problemName;
+
+    public String getProblemName() {
+        return problemName;
+    }
+
+    public AbstractPatient(String problemName) {
+        this.problemName = problemName;
+    }
 
     public AbstractPatient(int patientId, String patientName, int problemID) {
         this.patientId = patientId;
